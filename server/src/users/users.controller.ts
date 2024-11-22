b6f5b5448320e550ae381fbd4d4926eb9c5ad361
SottoMayor <ddsmmf@gmail.com>
2024-11-22 15:38:58 -0400

    Endpoints to list and create (in memory) users

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService: UsersService) {}
+
+    @Get()
+    listUsers() {
+        const users = this.userService.listUsers();
+        return users;
+    }
+
+    @Post()
+    addUser(@Body() userData: { name: string }) {
+        const user = this.userService.addUser(userData);
+        return user;
+    }
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [];
+
+    listUsers () {
+        return { users: this.users };
+    }
+
+    addUser(userData: { name: string }) {
+        const user = { id: Date.now(), name: userData.name };
+        this.users.push(user);
+        return { user };
+    }
+
+}
