acd066226716abc6293b595e7bea65dd8c8124b9
SottoMayor <ddsmmf@gmail.com>
2024-11-23 17:14:32 -0400

    Handle websocket CORS and logging about infos connecting

--- server/src/users/users.gateway.ts
+++ server/src/users/users.gateway.ts
@@ -1,11 +1,26 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'http';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: 'http://localhost:5173', // Substitua pelo endereço do seu frontend
+    methods: ['GET', 'POST'],
+    credentials: true,
+  } 
+})
 export class UsersGateway {
    @WebSocketServer() server: Server;
 
+  handleConnection(client: any) {
+    console.log(`Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client: any) {
+    console.log(`Cliente desconectado: ${client.id}`);
+  }
+
   notifyNewUser(user: { name: string, id: number }) {
+    console.log('Payload enviado: ', user)
     this.server.emit('newUser', { action: 'notify_new_user', ...user }); 
   }
 }
